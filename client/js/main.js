e8b70df53714623b1011adb0d6237670830fa76a
Nat Noordanus <n@natn.me>
2016-05-10 21:08:01 +0100

    Basic working demo with webpack and SceneJS

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,39 @@
-document.write("It works!");
+
+// Point SceneJS to the bundled plugins
+SceneJS.setConfigs({
+    pluginPath:"./plugins"
+});
+// Define scene
+var scene = SceneJS.createScene({
+  nodes:[
+    {
+      type:"lookAt",
+      eye:{ y:5, z:7 },
+      look:{ x:0, y:1, z:0 },
+      nodes:[
+        {
+          type:"material",
+          color:{ r:0.3, g:0.3, b:1.0 },
+          nodes:[
+            {
+              type:"rotate",
+              id:"myRotate",
+              y:1.0,
+              angle:0,
+              nodes:[
+                {
+                  type:"geometry/teapot"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+});
+// On each frame, spin the teapot a little bit
+scene.getNode("myRotate", (myRotate) => {
+  var angle = 0;
+  scene.on("tick", () => myRotate.setAngle(angle += 0.5));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const dir_client = path.resolve(__dirname, 'client');
-const dir_js     = path.resolve(dir_client, 'js');
-const dir_html   = path.resolve(dir_client, 'html');
-const dir_build  = path.resolve(dir_client, 'dist');
+const dir_client  = path.resolve(__dirname, 'client');
+const dir_js      = path.resolve(dir_client, 'js');
+const dir_html    = path.resolve(dir_client, 'html');
+const dir_scenejs = path.resolve(dir_client, 'vendor/scenejs');
+const dir_build   = path.resolve(dir_client, 'dist');
 
 module.exports = {
   entry: path.resolve(dir_js, 'main.js'),
@@ -20,16 +21,17 @@ module.exports = {
   },
   module: {
     loaders: [
-      {
-        loader: 'babel-loader',
-        test: dir_js,
-      }
+      { test: dir_js, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" }
     ]
   },
   plugins: [
     // Simply copies the files over
     new CopyWebpackPlugin([
-      { from: dir_html } // to: output.path
+      { from: dir_html }, // to: output.path
+      { from: dir_scenejs } // to: output.path
     ]),
     // Avoid publishing files when compilation fails
     new webpack.NoErrorsPlugin()
@@ -40,4 +42,7 @@ module.exports = {
   },
   // Create Sourcemaps for the bundle
   devtool: 'source-map',
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+  },
 };
